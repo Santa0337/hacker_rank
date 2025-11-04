ff3e0521fcf0f9d8d4b50a289e77a1a4e8eac220
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-04 12:41:16 +0530

    Add get_second_least_value.py for second least value
    Implement function to find the second least value in a set.

---
+++ Day1/get_second_least_value.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = set(map(int, input().split()))
+    l=sorted(arr)
+    print(l[-2])
+
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
