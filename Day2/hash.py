0c6edba551b4aeaf4e21feb1d3fe280542388aa8
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-05 12:08:21 +0530

    Add hash.py to compute hash of a tuple

---
+++ Day2/hash.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = tuple(map(int, raw_input().split()))
+    print(hash(integer_list))
+
+#https://www.hackerrank.com/challenges/python-tuples/problem?isFullScreen=true
