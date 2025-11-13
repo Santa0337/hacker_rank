20ae2ae0c4829ca37a5b583a04024355a014f769
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-13 10:49:06 +0530

    Add script for calculating powers of integers
    Implement a script to compute a^b + c^d.

---
+++ Day7/adding_two_pow.py
@@ -0,0 +1,7 @@
+a = int(input()) 
+b = int(input()) 
+c = int(input()) 
+d = int(input())
+print(a**b+c**d)
+
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
