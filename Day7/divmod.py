128c08cea71dab442ab5970559a4bcfd472798f8
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-13 10:44:46 +0530

    Add divmod function in divmod.py
    Implement divmod functionality to compute quotient and remainder.

---
+++ Day7/divmod.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+c=list(divmod(a,b))
+for i in c:
+    print(i)
+    
+print(divmod(a,b))
+
+
+#https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true
