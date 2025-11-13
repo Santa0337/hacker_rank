1364589dea63148c2094d4a14819130653768bfc
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-13 10:39:06 +0530

    Add right angle triangle with palindrome script
    Implement a script to print right angle triangle patterns with palindromic numbers.

---
+++ Day7/right_angle_triangle_with_palindrome.py
@@ -0,0 +1,9 @@
+for i in range(1,int(input())+1):
+    print(pow(10**i//9, 2))
+#https://www.hackerrank.com/challenges/triangle-quest-2/problem?isFullScreen=true
+
+#1
+#121
+#12321
+#1234321
+#123454321
