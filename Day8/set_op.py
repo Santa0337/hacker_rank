3eeef53a2be6c58456c008acc87470d08a01a9fa
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-14 15:14:43 +0530

    Add set intersection operation for two sets

---
+++ Day8/set_op.py
@@ -0,0 +1,9 @@
+n = int(input())
+Eng = set(map(int, input().split()))
+b = int(input())
+Frn = set(map(int, input().split()))
+
+print(len(Eng.intersection(Frn)))
+
+
+#https://www.hackerrank.com/challenges/py-set-intersection-operation/problem?isFullScreen=true
