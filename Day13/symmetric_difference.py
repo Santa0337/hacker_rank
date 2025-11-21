0a6a71f7cff563402462c1423e9e10bec1afd8fc
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-21 10:20:56 +0530

    Implement symmetric difference calculation

---
+++ Day13/symmetric_difference.py
@@ -0,0 +1,8 @@
+s=int(input())
+l=set(map(int,input().split()))
+t=int(input())
+m=set(map(int,input().split()))
+v=len(l^m)
+print(v)
+
+#https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem?isFullScreen=true
