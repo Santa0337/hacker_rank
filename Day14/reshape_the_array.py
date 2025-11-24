1c24f8fb6732e73d9aeb8c9730a4fd3fe6014a39
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-24 12:19:05 +0530

    Create reshape_the_array.py

---
+++ Day14/reshape_the_array.py
@@ -0,0 +1,6 @@
+import numpy as np 
+arr=list(map(int,input().split()))
+arr=np.array(arr)
+print(np.reshape(arr,(3,3)))
+
+#https://www.hackerrank.com/challenges/np-shape-reshape/problem?isFullScreen=true
