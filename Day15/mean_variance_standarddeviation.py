a65da16821a547c938412e46c73072f65e50c982
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-25 12:40:21 +0530

    Create mean_variance_standarddeviation.py

---
+++ Day15/mean_variance_standarddeviation.py
@@ -0,0 +1,11 @@
+import numpy
+n, m = map(int,input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+    
+print(numpy.mean(arr, axis = 1))
+print(numpy.var(arr, axis = 0))
+print(round(numpy.std(arr),11))
+
+#https://www.hackerrank.com/challenges/np-mean-var-and-std/problem?isFullScreen=true
