62afe1e504bbd80a98cb58a8bcf0f93b8f8f5b6d
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-26 12:42:00 +0530

    Create map_lambda.py

---
+++ Day16/map_lambda.py
@@ -0,0 +1,17 @@
+cube = lambda x: x**3
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    a,b=0,1
+    l=[]
+    for i in range(n):
+        l.append(a)
+        a,b=b,a+b
+    return l
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
+
+#https://www.hackerrank.com/challenges/map-and-lambda-expression/problem?isFullScreen=true
