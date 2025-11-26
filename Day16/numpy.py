5665df7db816b31c14b7b8bc6d81f31ab68d1091
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-11-26 13:06:50 +0530

    Create numpy.py

---
+++ Day16/numpy.py
@@ -0,0 +1,8 @@
+import numpy
+l=list(map(int,input().split()))
+a=numpy.zeros((l), dtype = int)
+b=numpy.ones((l), dtype = int)
+print(a)
+print(b)
+
+#https://www.hackerrank.com/challenges/np-zeros-and-ones/problem?isFullScreen=true
