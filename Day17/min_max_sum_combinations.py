612361d996184f1ed5103151943b256f62e3a12d
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-12-03 12:17:58 +0530

    Create min_max_sum_combinations.py

---
+++ Day17/min_max_sum_combinations.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    l=[]
+    for i in range(len(arr)):
+        l.append(sum(arr[:i]+arr[i+1:len(arr)]))
+    print(min(l),max(l))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
+#https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
