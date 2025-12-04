3b3127443dd098f77ed44d6682402d489db3250b
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-12-04 10:48:46 +0530

    Create grading.py

---
+++ Day18/grading.py
@@ -0,0 +1,28 @@
+
+import math
+import os
+import random
+import re
+import sys
+def gradingStudents(grades):
+    new_grades = [] 
+    for grade in grades: 
+        if grade <38: 
+            new_grades.append(grade) 
+        elif (5-(grade % 5))>=3 : 
+            new_grades.append(grade) 
+        else: 
+            new_grades.append(grade+(5-(grade % 5)))
+    return new_grades
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    grades_count = int(input().strip())
+    grades = []
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+    result = gradingStudents(grades)
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
+#https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
