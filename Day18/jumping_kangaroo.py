11dc9634662d69e31182022d69fbe7a504330868
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-12-04 12:08:17 +0530

    Create jumping_kangaroo.py

---
+++ Day18/jumping_kangaroo.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    j1=x1+v1
+    j2=x2+v2
+    for _ in range(10000):
+        if j1 == j2:
+            return 'YES'
+            break
+        else:
+            pass
+        j1+=v1
+        j2+=v2
+    else:
+        return 'NO'
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+#https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
