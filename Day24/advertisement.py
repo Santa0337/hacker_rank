2469f85daf142512bc9d9f709127de2b7d481ae2
C Dhanush <157220036+Santa0337@users.noreply.github.com>
2025-12-12 14:15:35 +0530

    Add viralAdvertising function in advertisement.py
    Implement viralAdvertising function to calculate likes based on shares.

---
+++ Day24/advertisement.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    likes = share = 2
+    for i in range(2, n+1):
+        share = (share * 3)//2
+        likes += share
+    return likes
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+#https://www.hackerrank.com/challenges/strange-advertising/problem?isFullScreen=true
